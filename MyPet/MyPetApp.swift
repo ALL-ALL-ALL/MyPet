728e8918fa5d06acf4bf5b7085fd09af1bdff379
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-10 18:20:34 +0200

    Initial Commit

---
+++ MyPet/MyPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPetApp.swift
+//  MyPet
+//
+//  Created by  Ixart on 10/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
