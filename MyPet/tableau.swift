5c9b2f288b6b48a98c6824197b368ecf39c1aa9b
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-17 02:33:52 +0200

    en cours......

---
+++ MyPet/tableau.swift
@@ -0,0 +1,17 @@
+//
+//  tableau.swift
+//  MyPet
+//
+//  Created by  Ixart on 17/04/2024.
+//
+
+import Foundation
+import UIKit
+
+
+class Animals: ObservableObject{
+    @Published var image : String = "" ?
+    @Published var name : String = "" ?
+
+    
+}
