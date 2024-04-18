f45d0d7e2dd23f2c861c8278b1a743b66c8feed8
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-19 01:59:10 +0200

    en cours........

---
+++ MyPet/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  MyPet
+//
+//  Created by  Ixart on 19/04/2024.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
