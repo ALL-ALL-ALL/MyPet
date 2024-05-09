72fe440d4c17c3b789dc7d433f90083a2704a603
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-05-10 00:25:20 +0200

    en cours.......

--- MyPet/ContentView.swift
+++ MyPet/ContentView.swift
@@ -11,37 +11,33 @@ import UIKit
 struct ContentView: View {
     
     @ObservedObject var animalStore = AnimalStore()
+    @State private var search = ""
+
 
     let columns: [GridItem] = [
         GridItem(.flexible(minimum: 150, maximum: .infinity)),
         GridItem(.flexible(minimum: 150, maximum: .infinity))
-
-
-
-
-
-
     ]
 
     var body: some View {
+        
+        NavigationStack{
+            ScrollView {
+                LazyVGrid(columns:columns) {
+                    ForEach(animalStore.animal) { animal in
+                        DetailsView(animal: animal)
+                        
+                    } // fin For Each
+                } // fin Lazygrid
+                .padding()
+                
+            } // fin scrollview
+            .navigationTitle("Animals")
             
-       
-                       
-               
-        ScrollView {
-            LazyVGrid(columns:columns) {
-                       ForEach(animalStore.animal) { animal in
-                           DetailsView(animal: animal)
+        } // fin navigationStack
+        .searchable(text: $search)
 
-                           
-                           
-                           
-                           
-                       }
-                   }
-                   .padding()
             
-               } // fin scrollview
            
     
         
