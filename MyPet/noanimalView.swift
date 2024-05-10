0b0d5057c0109860f1386a2d000b02dd4d77c157
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-05-11 01:16:56 +0200

    en cours......

---
+++ MyPet/noanimalView.swift
@@ -0,0 +1,18 @@
+//
+//  noanimalView.swift
+//  MyPet
+//
+//  Created by  Ixart on 11/05/2024.
+//
+
+import SwiftUI
+
+struct noanimalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    noanimalView()
+}
