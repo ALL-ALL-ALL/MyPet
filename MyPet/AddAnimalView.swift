bc248878bcdc30a9f379aa9449346a5d5f214b3e
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-05-14 00:36:36 +0200

    en cours.....

---
+++ MyPet/AddAnimalView.swift
@@ -0,0 +1,18 @@
+//
+//  AddAnimalView.swift
+//  MyPet
+//
+//  Created by  Ixart on 14/05/2024.
+//
+
+import SwiftUI
+
+struct AddAnimalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAnimalView()
+}
---
+++ MyPet/NoAnimalView.swift
@@ -0,0 +1,18 @@
+//
+//  NoAnimalView.swift
+//  MyPet
+//
+//  Created by  Ixart on 14/05/2024.
+//
+
+import SwiftUI
+
+struct NoAnimalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoAnimalView()
+}
